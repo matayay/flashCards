68523b7f827ae45e57b397cd41f909cdd26f7a67
Matayay Karuna <matayay2@gmail.com>
2023-02-28 02:01:13 -0700

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../stylesheets/Header.css'
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>The Ultimate Space Quiz</h1>
+            <h2>Test your knowledge on space!</h2>
+        </div>
+    )
+};
+
+export default Header;
