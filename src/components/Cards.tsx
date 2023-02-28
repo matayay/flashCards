0fae702a352007253214dc79b1a17a1feeaf4722
Matayay Karuna <matayay2@gmail.com>
2023-02-28 02:26:20 -0700

    created cards dictionary

---
+++ src/components/Cards.tsx
@@ -0,0 +1,14 @@
+const Cards = {
+    one:["Dark Energy", "Theorized substance which drives the accelerated expansion of the universe."],
+    two:["Dark Matter", "A form of matter that neither reflects nor absorbs light."],
+    three:["Age of Earth", "4 Billion Years"],
+    four:["Age of the Universe", "13.8 Billion Years"],
+    five:["Closest major galaxy to the Milky Way", "Andromeda"],
+    six:["Closest star to Earth", "Proxima Centauri"],
+    seven:["Speed of Light", "299 792 458 m / s"],
+    eight:["Cause of Earth's seasons", "The tilt upon its axis"],
+    nine:["Black hole at the center of the Milky Way", "Sagittarius A*"],
+    ten:["Name of the North Star", "Polaris"]
+};
+
+export default Cards;
