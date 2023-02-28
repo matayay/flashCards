5124d7ecd7ce5a16e7b4139e7052d0d02c41050c
Matayay Karuna <matayay2@gmail.com>
2023-02-28 03:37:54 -0700

    finished card flipping logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
 import Header from './components/Header'
+import FlashCard from './components/FlashCard';
+import { useState } from 'react'
 import './stylesheets/App.css'
 
 const App = () => {
+    const [flipped, setFlipped] = useState(0);
+    const updateFlipped = () => {
+        if (flipped == 1)
+        {
+            setFlipped(0);
+        }
+
+        else
+        {
+            setFlipped(1);
+        }
+    };
+
+    const [card, setCard] = useState(0);
+    const setFlashCard = () => {
+        setCard(Math.floor(Math.random() * 10))
+        setFlipped(0);
+    };
+
     return (
         <div className='App'>
             <Header />
+            <FlashCard flascard={card} flipped={flipped} updateFlipped={updateFlipped} />
+            <button onClick={setFlashCard}>New</button>
         </div>
     )
 };
---
+++ src/components/FlashCard.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import Cards from './Cards';
+
+interface Props {
+    flascard: number;
+    flipped: number;
+    updateFlipped: () => void;
+};
+
+const FlashCard:FC<Props> = (props) => {
+    const flashcards = 
+    [Cards.one, Cards.two, Cards.three, Cards.four, Cards.five, 
+    Cards.six, Cards.seven, Cards.eight, Cards.nine, Cards.ten]
+
+    return (
+        <div className='FlashCard' onClick={props.updateFlipped}>
+            <h3>{flashcards[props.flascard][props.flipped]}</h3>
+        </div>
+    )
+};
+
+export default FlashCard;
