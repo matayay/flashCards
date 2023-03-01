3d4969b7844a52a1d0c646820481fb29c1f23a58
Matayay Karuna <matayay2@gmail.com>
2023-02-28 17:47:28 -0700

    edited age of Earth

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,7 @@
 const Cards = {
     one:["Dark Energy", "Theorized substance which drives the accelerated expansion of the universe."],
     two:["Dark Matter", "A form of matter that neither reflects nor absorbs light."],
-    three:["Age of Earth", "4 Billion Years"],
+    three:["Age of Earth", "4.5 Billion Years"],
     four:["Age of the Universe", "13.8 Billion Years"],
     five:["Closest major galaxy to the Milky Way", "Andromeda"],
     six:["Closest star to Earth", "Proxima Centauri"],
