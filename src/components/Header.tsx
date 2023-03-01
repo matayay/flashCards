1bc51ceb3d9c35e71ad225361fc162fa089716f4
Matayay Karuna <matayay2@gmail.com>
2023-02-28 17:42:24 -0700

    created readme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,7 @@ const Header = () => {
         <div className="Header">
             <h1>The Ultimate Space Quiz</h1>
             <h2>Test your knowledge on space!</h2>
+            <h3>Cards: 10</h3>
         </div>
     )
 };
