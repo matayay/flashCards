5441311a1b9a8bd0ce0ec28b0542a0074d542c14
Matayay Karuna <matayay2@gmail.com>
2023-02-28 17:34:07 -0700

    got flipping animation

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -10,8 +10,15 @@ interface Props {
 
 const FlashCard:FC<Props> = (props) => {
     return (
-        <div className='FlashCard' onClick={props.updateFlipped}>
-            <h3>{flashcards[props.flascard][props.flipped]}</h3>
+        <div className='FlashCard'>
+            <div className='inner'>
+                <div className='front'>
+                    <h3>{flashcards[props.flascard][0]}</h3>
+                </div>
+                <div className='back'>
+                    <h3>{flashcards[props.flascard][1]}</h3>
+                </div>
+            </div>
         </div>
     )
 };
